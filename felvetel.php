3ea1346f2b3e081cdfe7478cafaed9340dce660d
Gabor Polgar <gabor.polgar@gmail.com>
2022-09-30 22:55:40 +0200

    bootstrap navigation with two elements

--- felvetel.php
+++ felvetel.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sportoló rögzítése</title>
+    <title>Játékok rögzítése</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg bg-info">
+        <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Játékok listázása</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="felvetel.php">Játék felvétele</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+  
     
 </body>
 </html>
